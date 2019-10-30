49e46b99ce258c7454bf01c82b2bde05c9453625
Luis Dominguez <reynaldodominguez@gmail.com>
2019-10-30 11:46:09 -0400

    Css File

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ var cheerio = require("cheerio");
 var db = require("./models");
 var PORT = 3000;
 var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/hwScraping", { useNewUrlParser: true });
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
