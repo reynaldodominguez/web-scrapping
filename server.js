247286e5340e6d5c357adf7997c017b60a3ba559
Luis Dominguez <reynaldodominguez@gmail.com>
2019-11-02 19:35:19 -0400

    MLab Fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 3000;
+var PORT =  process.env.PORT || 3000;
 var app = express();
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hwScraping";
 
