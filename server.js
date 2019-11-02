d7257ab9c7803850657bfc465543f35a99d80a93
Luis Dominguez <reynaldodominguez@gmail.com>
2019-11-02 19:26:59 -0400

    Heroku MLab Config

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var cheerio = require("cheerio");
 var db = require("./models");
 var PORT = 3000;
 var app = express();
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hwScraping";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/hwScraping", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 app.get("/scrape", function (req, res) {
 
